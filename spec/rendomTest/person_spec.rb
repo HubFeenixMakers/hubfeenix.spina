1ba0de6fcb9681f4b33bd4204734efd03d459768
Levon Stepanyan <levonmll@gmail.com>
2022-01-13 11:23:42 +0200

    add test for Retreats page title

--- spec/rendomTest/person_spec.rb
+++ spec/rendomTest/person_spec.rb
@@ -1,24 +1,6 @@
 require 'rails_helper'
-#require './person'
+require './spec/rendomTest/person'
 
-class Person
-	def initialize(name:, age:)
-	  @name = name.to_s
-	  @age = age.to_i
-	end
-  
-	def first_name
-	  @name.split(' ').first
-	end
-  
-	def last_name
-	  @name.split(' ').last
-	end
-  
-	def adult?
-	  @age >= 18
-	end
-  end
 RSpec.describe Person, type: :feature do
 	describe '#first_name' do
 	  it 'returns first part of the name' do
